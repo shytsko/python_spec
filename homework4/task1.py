e1cdb4e180c89b5150d669d44dd2bc7d1dd4b459
Vladimir Shytsko <shytsko.v@gmail.com>
2023-03-18 11:02:43 +0300

    Fix format

--- homework4/task1.py
+++ homework4/task1.py
@@ -2,10 +2,10 @@
 
 
 test_matrix = [[1, 2, 3, 4, 5],
-          [6, 7, 8, 9, 10],
-          [11, 12, 13, 14, 15],
-          [16, 17, 18, 19, 20],
-          [20, 21, 22, 23, 24]]
+               [6, 7, 8, 9, 10],
+               [11, 12, 13, 14, 15],
+               [16, 17, 18, 19, 20],
+               [20, 21, 22, 23, 24]]
 
 
 def transpose_matrix(matrix: list[list[int]]) -> bool:
@@ -17,6 +17,7 @@ def transpose_matrix(matrix: list[list[int]]) -> bool:
     for i in range(1, matrix_size):
         for j in range(0, i):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
 
 def print_matrix(matrix: list[list[int]]):
     for row in matrix:
