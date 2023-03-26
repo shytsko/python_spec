f462ec7fa09785eabca6bf25ca6cfd7c6f8ed5e6
Vladimir Shytsko <shytsko.v@gmail.com>
2023-03-26 20:24:25 +0300

    Add lesson 7

---
+++ lesson7/files.py
@@ -0,0 +1,28 @@
+# f = open("index.html", encoding="utf-8")
+#
+# print(*list(f))
+#
+# f.close()
+
+# f = open("bit_file.bin", "wb")
+# f.write("Привет, ".encode('utf-8') + "мир".encode('cp1251'))
+# f.close()
+#
+# f = open("bit_file.bin", "rb")
+# print(list(f))
+# f.close()
+
+# f = open("bit_file.bin", "r", encoding='utf-8')
+# print(list(f))
+# f.close()
+#
+# f = open("bit_file.bin", "r", encoding='utf-8', errors='replace')
+# print(list(f))
+# f.close()
+
+# with (
+#     open("index.html", "rt", encoding='utf-8') as f1,
+#     open("bit_file.bin", "rb") as f2
+# ):
+#     print(*list(f1))
+#     print(*list(f2))
