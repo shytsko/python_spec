7b080d0539822465300f3cc2b59ca4dd52ef43c6
Vladimir Shytsko <shytsko.v@gmail.com>
2023-03-28 19:51:33 +0300

    Update lesson 7

--- lesson7/files.py
+++ lesson7/files.py
@@ -27,9 +27,56 @@
 #     print(*list(f1))
 #     print(*list(f2))
 
-with open("index.html", "r+", encoding='utf-8') as f:
-    while res := f.readline():
-        print("---------------")
-        print(res)
-        print(f.tell())
+# with open("index.html", "r+", encoding='utf-8') as f:
+#     while res := f.readline():
+#         print("---------------")
+#         print(res)
+#         print(f.tell())
 
+# with open("index.html", "r+", encoding='utf-8') as f:
+#     f_it = iter(f)
+#     print(next(f_it), end="")
+#     print(next(f_it), end="")
+#     print(next(f_it), end="")
+#     print(next(f_it), end="")
+#     print(next(f_it), end="")
+#     print(*f_it, sep="", end="")
+
+
+import os
+from pathlib import Path
+
+# print(os.getcwd())
+# print(Path.cwd())
+# os.chdir("..")
+# print(Path.cwd())
+# os.chdir("lesson7")
+# print(Path.cwd())
+# os.mkdir("new_dir1")
+# Path("new_dir2").mkdir()
+
+# os.makedirs("os_mkdirs/1/2/4")
+# Path("path_mkdirs/5/6/10").mkdir(parents=True)
+# os.rmdir("os_mkdirs/1/2/4")
+# os.rmdir("os_mkdirs/1/2/3")
+# os.rmdir("os_mkdirs/1/2")
+# os.rmdir("os_mkdirs/1")
+#
+# Path("path_mkdirs/5/6/10").rmdir()
+# Path("path_mkdirs/5/6/7").rmdir()
+# Path("path_mkdirs/5/6").rmdir()
+# Path("path_mkdirs/5").rmdir()
+
+import shutil
+
+# Path(r"path_mkdirs\5\6\10").mkdir(parents=True)
+
+# with open(r"path_mkdirs\5\6\10\file.txt", "w", encoding="utf-8") as f:
+#     print("abracatabra\n", file=f)
+
+# shutil.rmtree("path_mkdirs")
+
+f1 = os.path.join(os.getcwd(), 'path_mkdir')
+print(f1)
+
+f2 = Path.cwd() / 'path_mkdir'
