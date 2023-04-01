ecb70185faeb4597e775f648d8cdf9ca830fa8f1
Vladimir Shytsko <shytsko.v@gmail.com>
2023-04-02 00:24:17 +0300

    Seminar 8 task 2

--- seminar8/task2.py
+++ seminar8/task2.py
@@ -7,6 +7,7 @@
 
 from pathlib import Path
 from json import dump, load
+from pprint import pprint
 
 LEVEL_MIN = 1
 LEVEL_MAX = 7
@@ -17,14 +18,25 @@ def input_data(file: str | Path) -> None:
         file = Path(file)
 
     if not file.exists():
-        data_dict = {str(level): [] for level in range(LEVEL_MIN, LEVEL_MAX + 1)}
+        data_dict = {str(level): {} for level in range(LEVEL_MIN, LEVEL_MAX + 1)}
     else:
         with file.open("r", encoding="utf-8") as f:
             data_dict = load(f)
 
-
-
     while True:
+        pprint(data_dict)
+        name = input("Введите имя: ")
+        if name == "":
+            break
+        id_ = input("Введите идентификатор: ")
+        if id_ in {i for users in data_dict.values() for i in users.keys()}:
+            print("Такой идентификатор уже есть")
+            continue
+        level = input("Введите уровень доступа: ")
+        if level not in data_dict.keys():
+            print("Уровень доступа должен быть от 1 до 7")
+            continue
+        data_dict[level][id_] = name
 
     with file.open("w", encoding="utf-8") as f:
         dump(data_dict, f, indent=2)
