2735452f23f2a3fcff343e17f53757fb3f7fab14
Vladimir Shytsko <shytsko.v@gmail.com>
2023-04-21 23:11:33 +0300

    Add homework 13

---
+++ homework13/task1.py
@@ -0,0 +1,23 @@
+# Возьмите 1-3 задачи из тех, что были на прошлых семинарах или в домашних заданиях. Напишите к ним
+# классы исключения с выводом подробной информации.
+
+
+class MyBaseException(Exception):
+    pass
+
+
+class IncorrectMatrixSize(MyBaseException):
+    def __init__(self, *, rows_expected=None, columns_expected=None):
+        self._rows_expected = rows_expected
+        self._columns_expected = columns_expected
+
+    def __str__(self):
+        if self._rows_expected is not None and self._columns_expected is not None:
+            return f"Размер матрицы справа должен быть {self._rows_expected} x {self._columns_expected}"
+        elif self._rows_expected is not None:
+            return f"Количество строк матрицы справа должно быть {self._rows_expected}"
+        elif self._columns_expected is not None:
+            return f"Количество столбцов матрицы справа должно быть {self._columns_expected}"
+        else:
+            return f"Не корректный размер матрицы справа"
+
